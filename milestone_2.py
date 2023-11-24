d75760cdb4c9e19e47588e824f3d8154df1d2540
Skanwal1 <150562580+Skanwal1@users.noreply.github.com>
2023-11-24 00:38:49 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,22 +1,2 @@
-import random
-
-fav_fruits = ["Apple","Grapes", "Mango", "Orange", "Strawberry" ]
-
-word_list = fav_fruits
-
-word = random.choice(word_list)
-
-print("Favourite Fruits List : ", word_list)
-
-print("Random Fruit : " , word)
-
-guess = input("Enter a letter : ")
-
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
-
-
 
 
